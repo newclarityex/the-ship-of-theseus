7b480de478db2778c3598dd23c5bcdc1e484f691
Kira Lu <newclearex@gmail.com>
2024-03-31 21:22:54 -0400

    spear buff

--- src/core/items/mod.rs
+++ src/core/items/mod.rs
@@ -144,8 +144,8 @@ pub fn trigger_weapons(
                     ActiveCollisionTypes::STATIC_STATIC,
                     ActiveEvents::COLLISION_EVENTS,
                     ContactWeapon {
-                        pierce: 1 + player_leveling.pierce,
-                        damage: 15. * player_leveling.damage_multiplier,
+                        pierce: 2 + player_leveling.pierce,
+                        damage: 25. * player_leveling.damage_multiplier,
                     },
                     SpearBehavior {
                         angle: throw_angle,
