ff26d8890f956bf4e0d935734d0a0ca6cc3e4aa0
Kira Lu <newclearex@gmail.com>
2024-04-01 11:05:55 -0400

    add proxy

--- wasm/restart-audio-context.js
+++ wasm/restart-audio-context.js
@@ -3,6 +3,16 @@
     // An array of all contexts to resume on the page
     const audioContextList = [];
 
+    // A proxy object to intercept AudioContexts and
+    // add them to the array for tracking and resuming later
+    self.AudioContext = new Proxy(self.AudioContext, {
+        construct(target, args) {
+            const result = new target(...args);
+            audioContextList.push(result);
+            return result;
+        },
+    });
+
     setInterval(() => {
         audioContextList = audioContextList.filter(context => context.state !== 'running');
         audioContextList.forEach(context => {
